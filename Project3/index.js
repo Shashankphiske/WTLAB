964bc9f70d3b4c107fedf35e850ee4ad7ef5d2fb
ShashankPhiske <shashankphiske@gmail.com>
2025-02-28 15:05:16 +0530

    Project 4

--- Project3/index.js
+++ Project3/index.js
@@ -1,1 +1,22 @@
-console.log("hi1");
+function register(){
+    const name = document.getElementById("name").value; 
+    const email = document.getElementById("email").value;
+    const className = document.getElementById("class").value;
+    const rollno = document.getElementById("rollno").value;
+
+    if(!name || !email || !className || !rollno){
+        alert("Please enter all the fields");
+    }
+
+    
+}
+
+function namechange(){
+    let newval = document.getElementById("name").value;
+    console.log(newval);
+    if(parseInt(newval[newval.length-1]) || parseInt(newval[0])){
+        alert("Please enter only alphabets inside the name fields");
+        newval = "";
+        document.getElementById("name").value = newval;
+    }
+}
